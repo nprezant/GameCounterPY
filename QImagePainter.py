9301fb2f307c1dd2b168c2cc68d0a369a740e037
nprezant <34352369+nprezant@users.noreply.github.com>
2019-09-03 11:32:56 +0200

    Fix oval drawing and translation movement

--- QImagePainter.py
+++ QImagePainter.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import Qt, QRectF, QMarginsF
-from PyQt5.QtGui import QImage, QPixmap, QPalette, QPainter, QWheelEvent, QKeyEvent, QIcon
+from PyQt5.QtGui import QImage, QPixmap, QPalette, QPainter, QWheelEvent, QKeyEvent, QIcon, QPen, QColor
 from PyQt5.QtWidgets import (
     QGraphicsView, QGraphicsScene, QGraphicsItem, QToolBar, QAction
 )
@@ -13,26 +13,42 @@ class QImagePainter(QGraphicsView):
         self.setRenderHint(QPainter.Antialiasing)
 
         self.mainPixmapItem = self.scene.addPixmap(QPixmap())
-        # self.mainPixmapItem.ItemIsMovable = True
 
-        # self.DragMode = self.ScrollHandDrag
+        # self.mainPixmapItem.ItemIsMovable = True
+        # self.setDragMode = QGraphicsView.ScrollHandDrag
+
+        # policies
+        # self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
+        # self.setResizeAnchor(QGraphicsView.AnchorUnderMouse)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
 
         self.toolbar = QToolBar()
         self.initToolbar()
+
+        self._pen = QPen(QColor(50, 150, 230))
+        self._pen.setWidth(3)
 
         self._drawStartPos = None
         self._dynamicOval = None
         self._drawnItems = []
 
     def setMainPixmapFromPath(self, imgPath):
+
+        # set image
         image = QImage(str(imgPath))
         pixmap = self.mainPixmapItem.pixmap()
         pixmap.convertFromImage(image)
         self.mainPixmapItem.setPixmap(pixmap)
-        # TODO set the scene rect here from the image or pixmap rect
-        # boundingRect = self.mainPixmapItem.boundingRect()
-        # boundingRect += QMarginsF(10,10,10,10)
-        # self.scene.setSceneRect(boundingRect)
+
+        # set scene rect
+        boundingRect = self.mainPixmapItem.boundingRect()
+        margin = 2000
+        boundingRect += QMarginsF(margin,margin,margin,margin)
+        self.scene.setSceneRect(boundingRect)
+
+        # center on image
+        self.centerOn(self.mainPixmapItem)
 
     def wheelEvent(self, event: QWheelEvent):
         self.scaleView(2 ** float(-event.angleDelta().y()/280))
@@ -40,23 +56,29 @@ class QImagePainter(QGraphicsView):
     def keyPressEvent(self, event: QKeyEvent):
         key = event.key()
         if key == Qt.Key_Up:
-            # self.translate(0,-10)
-            self.mainPixmapItem.moveBy(0,-10)
+            self.translateVertical(-10)
         elif key == Qt.Key_Down:
-            self.mainPixmapItem.moveBy(0,10)
+            self.translateVertical(10)
         elif key == Qt.Key_Left:
-            self.mainPixmapItem.moveBy(-10,0)
+            self.translateHorizontal(-10)
         elif key == Qt.Key_Right:
-            self.mainPixmapItem.moveBy(10,0)
+            self.translateHorizontal(10)
         elif key == Qt.Key_Space:
-            # TODO this doesn't work.. why?
             self.centerOn(self.mainPixmapItem)
         else:
             QGraphicsView().keyPressEvent(event)
 
+    def translateVertical(self, dy):
+        bar = self.verticalScrollBar()
+        bar.setValue(bar.value() + dy)
+
+    def translateHorizontal(self, dx):
+        bar = self.horizontalScrollBar()
+        bar.setValue(bar.value() + dx)
+
     def scaleView(self, scaleFactor):
         factor = self.transform().scale(scaleFactor, scaleFactor).mapRect(QRectF(0, 0, 1, 1)).width()
-        if (factor < 0.07 or factor > 100):
+        if (factor < 0.1 or factor > 100):
             return
 
         self.scale(scaleFactor, scaleFactor)
@@ -64,11 +86,14 @@ class QImagePainter(QGraphicsView):
     def mousePressEvent(self, event):
         self._drawStartPos = None
         if self.ovalModeAct.isChecked():
-            if self.itemAt(event.pos()) == self.mainPixmapItem: 
+            if self.mainPixmapItem.isUnderMouse():
                 self._drawStartPos = self.mapToScene(event.pos())
                 self._dynamicOval = self.scene.addEllipse(
-                    QRectF(self._drawStartPos.x(), self._drawStartPos.y(), 1, 1)
+                    QRectF(self._drawStartPos.x(), self._drawStartPos.y(), 1, 1),
+                    self._pen
                 )
+        else:
+            super().mousePressEvent(event)
 
     def mouseMoveEvent(self, event):
         if self._dynamicOval:
@@ -77,11 +102,15 @@ class QImagePainter(QGraphicsView):
                 QRectF(self._drawStartPos.x(), self._drawStartPos.y(),
                 pos.x() - self._drawStartPos.x(), pos.y() - self._drawStartPos.y())
             )
+        else:
+            super().mouseMoveEvent(event)
 
     def mouseReleaseEvent(self, event):
         if self._dynamicOval:
             self._drawnItems.append(self._dynamicOval)
             self._dynamicOval = None
+        else:
+            super().mouseReleaseEvent(event)
 
     def toggleSelectionMode(self):
         if self.selectionModeAct.isChecked():
