f0062024d21c0e48915e17db56e8bb52fb645c09
nprezant <34352369+nprezant@users.noreply.github.com>
2019-09-04 15:52:55 +0200

    Smoothed panning (arrow keys) and zooming (mouse)

--- QImagePainter.py
+++ QImagePainter.py
@@ -1,4 +1,4 @@
-from PyQt5.QtCore import Qt, QRectF, QMarginsF
+from PyQt5.QtCore import Qt, QRectF, QMarginsF, QTimeLine
 from PyQt5.QtGui import QImage, QPixmap, QPalette, QPainter, QWheelEvent, QKeyEvent, QIcon, QPen, QColor
 from PyQt5.QtWidgets import (
     QGraphicsView, QGraphicsScene, QGraphicsItem, QToolBar, QAction
@@ -33,6 +33,10 @@ class QImagePainter(QGraphicsView):
         self._dynamicOval = None
         self._drawnItems = []
 
+        self._numScheduledScalings = 0
+        self._numScheduledHTranslations = 0
+        self._numScheduledVTranslations = 0
+
     def setMainPixmapFromPath(self, imgPath):
 
         # set image
@@ -55,22 +59,43 @@ class QImagePainter(QGraphicsView):
 
     def bestFitImage(self):
         self.fitInView(self.mainPixmapItem, Qt.KeepAspectRatio)
-        # self.centerImage()
 
     def wheelEvent(self, event: QWheelEvent):
-        scaleFactor = 2 ** float(-event.angleDelta().y()/280)
-        self.scaleView(scaleFactor)
+        numDegrees = event.angleDelta().y() / 8
+        numSteps = numDegrees / 15
+        self._numScheduledScalings += numSteps
+
+        if (self._numScheduledScalings * numSteps < 0): # if user moved the wheel in another direction, we reset previously scheduled scalings
+            self._numScheduledScalings = numSteps
+
+        anim = QTimeLine(350, self)
+        anim.setUpdateInterval(20)
+
+        anim.valueChanged.connect(self.scalingTime)
+        anim.finished.connect(self.scaleAnimFinished)
+        anim.start()
+
+    def scalingTime(self, x):
+        factor = 1 + self._numScheduledScalings / 300.0
+        self.scaleView(factor)
+
+    def scaleAnimFinished(self):
+        if (self._numScheduledScalings > 0):
+            self._numScheduledScalings -= 1
+        else:
+            self._numScheduledScalings += 1
+        self.sender().deleteLater()
 
     def keyPressEvent(self, event: QKeyEvent):
         key = event.key()
         if key == Qt.Key_Up:
-            self.translateVertical(-10)
+            self.translateVerticalEvent(-100)
         elif key == Qt.Key_Down:
-            self.translateVertical(10)
+            self.translateVerticalEvent(100)
         elif key == Qt.Key_Left:
-            self.translateHorizontal(-10)
+            self.translateHorizontalEvent(-100)
         elif key == Qt.Key_Right:
-            self.translateHorizontal(10)
+            self.translateHorizontalEvent(100)
         elif key == Qt.Key_Space:
             self.bestFitImage()
         else:
@@ -84,9 +109,59 @@ class QImagePainter(QGraphicsView):
         bar = self.horizontalScrollBar()
         bar.setValue(bar.value() + dx)
 
+    def translateHorizontalEvent(self, dx):
+        numSteps = dx * 2
+        self._numScheduledHTranslations += numSteps
+
+        if (self._numScheduledHTranslations * numSteps < 0): # if user moved the wheel in another direction, we reset previously scheduled scalings
+            self._numScheduledHTranslations = numSteps
+
+        anim = QTimeLine(350, self)
+        anim.setUpdateInterval(20)
+
+        anim.valueChanged.connect(self.translateHTime)
+        anim.finished.connect(self.translateHAnimFinished)
+        anim.start()
+
+    def translateHTime(self, x):
+        dx = self._numScheduledHTranslations / 200
+        self.translateHorizontal(dx)
+
+    def translateHAnimFinished(self):
+        if (self._numScheduledHTranslations > 0):
+            self._numScheduledHTranslations -= 1
+        else:
+            self._numScheduledHTranslations += 1
+        self.sender().deleteLater()
+
+    def translateVerticalEvent(self, dy):
+        numSteps = dy * 2
+        self._numScheduledVTranslations += numSteps
+
+        if (self._numScheduledVTranslations * numSteps < 0): # if user moved the wheel in another direction, we reset previously scheduled scalings
+            self._numScheduledVTranslations = numSteps
+
+        anim = QTimeLine(350, self)
+        anim.setUpdateInterval(20)
+
+        anim.valueChanged.connect(self.translateVTime)
+        anim.finished.connect(self.translateVAnimFinished)
+        anim.start()
+
+    def translateVTime(self, y):
+        dy = self._numScheduledVTranslations / 200
+        self.translateVertical(dy)
+
+    def translateVAnimFinished(self):
+        if (self._numScheduledVTranslations > 0):
+            self._numScheduledVTranslations -= 1
+        else:
+            self._numScheduledVTranslations += 1
+        self.sender().deleteLater()
+
     def scaleView(self, scaleFactor):
         factor = self.transform().scale(scaleFactor, scaleFactor).mapRect(QRectF(0, 0, 1, 1)).width()
-        if (factor < 0.1 or factor > 100):
+        if (factor < 0.8 or factor > 100):
             return
 
         self.scale(scaleFactor, scaleFactor)
