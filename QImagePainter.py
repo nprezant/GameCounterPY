33a2f8f64114ebaa68c9bbc3664f8c4a453f7beb
nprezant <34352369+nprezant@users.noreply.github.com>
2019-09-04 22:47:32 +0200

    Smoothed out some scrolling trasitions

--- QImagePainter.py
+++ QImagePainter.py
@@ -4,64 +4,53 @@ from PyQt5.QtWidgets import (
     QGraphicsView, QGraphicsScene, QGraphicsItem, QToolBar, QAction
 )
 
-class QImagePainter(QGraphicsView):
+class QSmoothGraphicsView(QGraphicsView):
+    '''Implements smooth mouse/keyboard navigation'''
+
     def __init__(self):
         super().__init__()
-
-        self.scene = QGraphicsScene(self)
-        self.setScene(self.scene)
-        self.setRenderHint(QPainter.Antialiasing)
-
-        self.mainPixmapItem = self.scene.addPixmap(QPixmap())
-
-        # self.mainPixmapItem.ItemIsMovable = True
-        # self.setDragMode = QGraphicsView.ScrollHandDrag
-
-        # policies
-        # self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
-        # self.setResizeAnchor(QGraphicsView.AnchorUnderMouse)
-        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-
-        self.toolbar = QToolBar()
-        self.initToolbar()
-
-        self._pen = QPen(QColor(50, 150, 230))
-        self._pen.setWidth(10)
-
-        self._drawStartPos = None
-        self._dynamicOval = None
-        self._drawnItems = []
-
+        
         self._numScheduledScalings = 0
         self._numScheduledHTranslations = 0
         self._numScheduledVTranslations = 0
 
-    def setMainPixmapFromPath(self, imgPath):
+        self.animatingH = False
+        self.animatingV = False
 
-        # set image
-        image = QImage(str(imgPath))
-        pixmap = self.mainPixmapItem.pixmap()
-        pixmap.convertFromImage(image)
-        self.mainPixmapItem.setPixmap(pixmap)
+        self.controlDown = False
 
-        # set scene rect
-        boundingRect = self.mainPixmapItem.boundingRect()
-        margin = boundingRect.width() * 2
-        boundingRect += QMarginsF(margin,margin,margin,margin)
-        self.scene.setSceneRect(boundingRect)
+    def keyPressEvent(self, event: QKeyEvent):
+        key = event.key()
+        if key == Qt.Key_Up:
+            self.translateVerticalEvent(-100)
+        elif key == Qt.Key_Down:
+            self.translateVerticalEvent(100)
+        elif key == Qt.Key_Left:
+            self.translateHorizontalEvent(-100)
+        elif key == Qt.Key_Right:
+            self.translateHorizontalEvent(100)
+        elif key == Qt.Key_Control:
+            print('control is down')
+            self.controlDown = True
+            super().keyPressEvent(event)
+        else:
+            super().keyPressEvent(event)
 
-        # manage view
-        self.bestFitImage()
-
-    def centerImage(self):
-        self.centerOn(self.mainPixmapItem)
-
-    def bestFitImage(self):
-        self.fitInView(self.mainPixmapItem, Qt.KeepAspectRatio)
+    def keyReleaseEvent(self, event: QKeyEvent):
+        key = event.key()
+        if key == Qt.Key_Control:
+            print('control is up')
+            self.controlDown = False
+        super().keyReleaseEvent(event)
 
     def wheelEvent(self, event: QWheelEvent):
-        numDegrees = event.angleDelta().y() / 8
+        print(self.controlDown)
+        if self.controlDown:
+            self.zoom(event.angleDelta().y() / 8)
+        else:
+            super().wheelEvent(event)
+
+    def zoom(self, numDegrees):
         numSteps = numDegrees / 15
         self._numScheduledScalings += numSteps
 
@@ -86,21 +75,6 @@ class QImagePainter(QGraphicsView):
             self._numScheduledScalings += 1
         self.sender().deleteLater()
 
-    def keyPressEvent(self, event: QKeyEvent):
-        key = event.key()
-        if key == Qt.Key_Up:
-            self.translateVerticalEvent(-100)
-        elif key == Qt.Key_Down:
-            self.translateVerticalEvent(100)
-        elif key == Qt.Key_Left:
-            self.translateHorizontalEvent(-100)
-        elif key == Qt.Key_Right:
-            self.translateHorizontalEvent(100)
-        elif key == Qt.Key_Space:
-            self.bestFitImage()
-        else:
-            super().keyPressEvent(event)
-
     def translateVertical(self, dy):
         bar = self.verticalScrollBar()
         bar.setValue(bar.value() + dy)
@@ -110,21 +84,25 @@ class QImagePainter(QGraphicsView):
         bar.setValue(bar.value() + dx)
 
     def translateHorizontalEvent(self, dx):
-        numSteps = dx * 2
+        numSteps = dx * 1
         self._numScheduledHTranslations += numSteps
 
         if (self._numScheduledHTranslations * numSteps < 0): # if user moved the wheel in another direction, we reset previously scheduled scalings
             self._numScheduledHTranslations = numSteps
 
-        anim = QTimeLine(350, self)
-        anim.setUpdateInterval(20)
+        if not self.animatingH:
+            anim = QTimeLine(350, self)
+            anim.setUpdateInterval(20)
 
-        anim.valueChanged.connect(self.translateHTime)
-        anim.finished.connect(self.translateHAnimFinished)
-        anim.start()
+            anim.valueChanged.connect(self.translateHTime)
+            anim.finished.connect(self.translateHAnimFinished)
+            anim.start()
 
     def translateHTime(self, x):
-        dx = self._numScheduledHTranslations / 200
+        if self._numScheduledHTranslations > 0:
+            dx = 0.75
+        else:
+            dx = -0.75
         self.translateHorizontal(dx)
 
     def translateHAnimFinished(self):
@@ -133,6 +111,7 @@ class QImagePainter(QGraphicsView):
         else:
             self._numScheduledHTranslations += 1
         self.sender().deleteLater()
+        self.animatingH = False
 
     def translateVerticalEvent(self, dy):
         numSteps = dy * 2
@@ -141,15 +120,19 @@ class QImagePainter(QGraphicsView):
         if (self._numScheduledVTranslations * numSteps < 0): # if user moved the wheel in another direction, we reset previously scheduled scalings
             self._numScheduledVTranslations = numSteps
 
-        anim = QTimeLine(350, self)
-        anim.setUpdateInterval(20)
+        if not self.animatingV:
+            anim = QTimeLine(350, self)
+            anim.setUpdateInterval(20)
 
-        anim.valueChanged.connect(self.translateVTime)
-        anim.finished.connect(self.translateVAnimFinished)
-        anim.start()
+            anim.valueChanged.connect(self.translateVTime)
+            anim.finished.connect(self.translateVAnimFinished)
+            anim.start()
 
     def translateVTime(self, y):
-        dy = self._numScheduledVTranslations / 200
+        if self._numScheduledVTranslations > 0:
+            dy = 0.75
+        else:
+            dy = -0.75
         self.translateVertical(dy)
 
     def translateVAnimFinished(self):
@@ -158,13 +141,80 @@ class QImagePainter(QGraphicsView):
         else:
             self._numScheduledVTranslations += 1
         self.sender().deleteLater()
+        self.animatingV = False
 
     def scaleView(self, scaleFactor):
         factor = self.transform().scale(scaleFactor, scaleFactor).mapRect(QRectF(0, 0, 1, 1)).width()
-        if (factor < 0.8 or factor > 100):
+        if (factor < 0.02 or factor > 100):
             return
 
         self.scale(scaleFactor, scaleFactor)
+    
+
+class QImagePainter(QSmoothGraphicsView):
+    def __init__(self):
+        super().__init__()
+
+        self.scene = QGraphicsScene(self)
+        self.setScene(self.scene)
+        self.setRenderHint(QPainter.Antialiasing)
+
+        self.mainPixmapItem = self.scene.addPixmap(QPixmap())
+        self.setDragMode(QGraphicsView.ScrollHandDrag)
+
+        # policies
+        # self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
+        # self.setResizeAnchor(QGraphicsView.AnchorUnderMouse)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+        self.toolbar = QToolBar()
+        self.initToolbar()
+
+        self._pen = QPen(QColor(50, 150, 230))
+        self._pen.setWidth(10)
+
+        self._drawStartPos = None
+        self._dynamicOval = None
+        self._drawnItems = []
+
+    def setMainPixmapFromPath(self, imgPath):
+
+        # set image
+        image = QImage(str(imgPath))
+        pixmap = self.mainPixmapItem.pixmap()
+        pixmap.convertFromImage(image)
+        self.mainPixmapItem.setPixmap(pixmap)
+
+        # set scene rect
+        boundingRect = self.mainPixmapItem.boundingRect()
+        margin = 100 # boundingRect.width() * 2
+        boundingRect += QMarginsF(margin,margin,margin,margin)
+        self.scene.setSceneRect(boundingRect)
+
+        # manage view
+        self.bestFitImage()
+
+    def centerImage(self):
+        self.centerOn(self.mainPixmapItem)
+
+    def bestFitImage(self):
+        self.fitInView(self.mainPixmapItem, Qt.KeepAspectRatio)
+
+    def keyPressEvent(self, event: QKeyEvent):
+        key = event.key()
+        if key == Qt.Key_Space:
+            self.bestFitImage()
+        else:
+            super().keyPressEvent(event)
+
+    # def scaleView(self, scaleFactor):
+    #     rect = self.mainPixmapItem.pixmap().scale(scaleFactor, scaleFactor).mapRect(QRectF(0, 0, 1, 1))
+    #     print(f'width: {rect.width()}')
+    #     print(f'height: {rect.height()}')
+    #     if rect.width() < self.width() \
+    #         or rect.height() < self.height():
+    #         return
 
     def mousePressEvent(self, event):
         self._drawStartPos = None
